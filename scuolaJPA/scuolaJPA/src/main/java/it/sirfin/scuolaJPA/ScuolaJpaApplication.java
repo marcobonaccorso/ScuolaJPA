2efae3de70d6475342bf11c50d13992abaeb4959
marcobonaccorso <marco.bonaccorso00@gmail.com>
2021-02-11 14:28:06 +0100

    primo commit

---
+++ scuolaJPA/scuolaJPA/src/main/java/it/sirfin/scuolaJPA/ScuolaJpaApplication.java
@@ -0,0 +1,13 @@
+package it.sirfin.scuolaJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScuolaJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScuolaJpaApplication.class, args);
+	}
+
+}
---
+++ scuolaJPA/scuolaJPA/src/test/java/it/sirfin/scuolaJPA/ScuolaJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sirfin.scuolaJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScuolaJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
